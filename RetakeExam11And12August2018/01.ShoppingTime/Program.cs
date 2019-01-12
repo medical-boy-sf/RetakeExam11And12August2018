5a2e64471f53c3296a9c0b795549a88f21f3b263
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-12 19:58:42 +0200

    01. Shopping Time

--- RetakeExam11And12August2018/01.ShoppingTime/Program.cs
+++ RetakeExam11And12August2018/01.ShoppingTime/Program.cs
@@ -6,7 +6,19 @@ namespace _01.ShoppingTime
     {
         static void Main()
         {
+            int time = int.Parse(Console.ReadLine());
+            double pricePeripheral = double.Parse(Console.ReadLine());
+            double priceSoftware = double.Parse(Console.ReadLine());
+            double priceFrappe = double.Parse(Console.ReadLine());
 
+            time -= 5;
+            time -= 3 * 2;
+            time -= 2 * 2;
+
+            double totalSum = 3 * pricePeripheral + 2 * priceSoftware + priceFrappe;
+            Console.WriteLine($"{totalSum:F2}");
+            Console.WriteLine(time);
+            
         }
     }
 }
